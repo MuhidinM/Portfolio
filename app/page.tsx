5b615de0a1200b510e0db89f9fbffd0895127edd
MuhidinM <misbahmuhidin3@gmail.com>
2024-01-27 16:35:51 +0300

    achievement section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
+import AchievementsSection from "@/components/achivement-section";
 import HeroSection from "@/components/hero-section";
 import Navbar from "@/components/navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-      <HeroSection />
+        <HeroSection />
+        <AchievementsSection />
       </div>
     </main>
   );
