7f85b66e476e8dca770301617b5aa163611188de
MuhidinM <misbahmuhidin3@gmail.com>
2024-01-27 19:26:45 +0300

    about and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import AchievementsSection from "@/components/achivement-section";
+import AboutSection from "@/components/about-section";
+import AchievementsSection from "@/components/achievement-section";
+import Footer from "@/components/footer";
 import HeroSection from "@/components/hero-section";
 import Navbar from "@/components/navbar";
 
@@ -9,7 +11,9 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AchievementsSection />
+        <AboutSection />
       </div>
+      <Footer/>
     </main>
   );
 }
