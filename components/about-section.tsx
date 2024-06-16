c7668a1427fce658b0106fa8ae43166f3445a835
MuhidinM <misbahmuhidin3@gmail.com>
2024-06-16 15:57:49 +0300

    corrected images, styles and lot more

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -16,13 +16,8 @@ const AboutSection = () => {
 
   return (
     <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image
-          src="/images/about-image.jpg"
-          width={500}
-          height={500}
-          alt="about"
-        />
+      <div className="md:grid md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/code.gif" width={800} height={800} alt="about" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">{aboutMe}</p>
